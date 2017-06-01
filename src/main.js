637860d4440f0a360a506b271221057b84a52ed8
huangchucai <425205839@qq.com>
2017-06-01 23:56:28 +0800

    提交饿了么代码

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import seller from 'components/seller/seller';
+import ratings from 'components/ratings/ratings';
+
+import 'common/stylus/index.styl';
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+/* eslint-disable no-new */
+let app = Vue.extend(App); // 挂载的App.vue组件
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/seller': {
+    component: seller
+  },
+  '/ratings': {
+    component: ratings
+  }
+});
+router.start(app, '#app');
+router.go('/goods');
