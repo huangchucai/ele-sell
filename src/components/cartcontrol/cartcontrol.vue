c9f7b38467502ac8feea739c050707d56b316d26
huangchucai <425205839@qq.com>
2017-06-04 22:46:41 +0800

    提交食物添加的相应功能

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,36 +1,80 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
+    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart" transition="move">
+      <span class="icon-remove_circle_outline inner"></span>
+    </div>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
 </template>
 <script type="text/ecmascript-6">
+  import Vue from 'vue';
   export default {
     props: {
       food: {
         type: Object
       }
     },
-    created() {
-      console.log(this.food);
-    },
     methods: {
-      addCart(){}
+      addCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+        this.$dispatch('cart.add', event.target);
+      },
+      decreaseCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (this.food.count) {
+          this.food.count--;
+        }
+      }
     }
   };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-.cartcontrol
-  font-size 0
-  .cart-decrease, .cart-add
+  .cartcontrol
+    font-size 0
+    .cart-decrease
+      display inline-block
+      padding 6px
+      transition all 0.4s linear
+      &.move-transition
+        opacity 1
+        transform translate3D(0, 0, 0)
+        .inner
+          display inline-block
+          font-size 24px
+          line-height 24px
+          color rgb(0, 160, 220)
+          transition all 0.4s linear
+          transform rotate(0)
+      &.move-enter, &.move-leave
+        opacity 0
+        transform translate3D(24px, 0, 0)
+        .inner
+          transform rotate(180deg)
+
+  .cart-count
+    display inline-block
+    vertical-align top
+    width 12px
+    font-size 10px
+    padding-top 6px
+    line-height 24px
+    text-align center
+    color rgb(147, 153, 159)
+
+  .cart-add
     display inline-block
     padding 6px
     font-size 24px
     line-height 24px
-    color rgb(0,160,220)
-  .cart-count
-    display inline-block
-  .cart-add
-    display inline-block
+    color rgb(0, 160, 220)
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item" :class="{current: currentIndex===$index }" @click="selectMenu($index,$event)">
+        <li v-for="item in goods" class="menu-item" :class="{current: currentIndex===$index }"
+            @click="selectMenu($index,$event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -10,7 +11,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"  v-el:food-wrapper>
+    <div class="foods-wrapper" v-el:food-wrapper>
       <ul>
         <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
@@ -37,7 +38,7 @@
         </li>
       </ul>
     </div>
-    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -69,6 +70,17 @@
           }
         }
         return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
       }
     },
     created() {
@@ -92,13 +104,19 @@
         let foodList = this.$els.foodWrapper.getElementsByClassName('food-list-hook');
         let el = foodList[index];
         this.foodsScroll.scrollToElement(el, 300);
-        console.log(index);
+      },
+      _drop(target) {
+        // 优化性能，异步执行下拉动画
+        this.$nextTick(() => {
+          this.$refs.shopcart.drop(target);
+        });
       },
       _initScroll() {
         this.meunScroll = new BScroll(this.$els.menuWrapper, {
           click: true
         });
         this.foodsScroll = new BScroll(this.$els.foodWrapper, {
+          click: true,
           probeType: 3
         });
         this.foodsScroll.on('scroll', (pos) => {
@@ -119,6 +137,11 @@
     components: {
       shopcart,
       cartcontrol
+    },
+    events: {
+      'cart.add'(target) {
+        this._drop(target);
+      }
     }
   };
 </script>
@@ -142,13 +165,13 @@
         line-height 14px
         padding 0 12px
         &.current
-          position  relative
+          position relative
           z-index 10
           margin-top -1px
           background #fff
           font-weight 700
           .text
-             border-none()
+            border-none()
         .icon
           display inline-block
           vertical-align top
@@ -171,7 +194,7 @@
           display table-cell
           width 56px
           vertical-align middle
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
           font-size 12px
     .foods-wrapper
       flex 1
@@ -181,13 +204,13 @@
         line-height 26px
         border-left 2px solid #d9dde1
         font-size 12px
-        color rgb(147,153,159)
+        color rgb(147, 153, 159)
         background #f3f5f7
       .food-item
         display flex
         margin 18px
         padding-bottom 18px
-        border-1px(rgba(7,17,27,0.1))
+        border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
           margin-bottom 0
@@ -201,7 +224,7 @@
             height 14px
             line-height 14px
             font-size 14px
-            color rgb(7,17,27)
+            color rgb(7, 17, 27)
           .desc, .extra
             line-height: 10px
             font-size: 10px
@@ -213,16 +236,16 @@
             .count
               margin-right: 12px
           .price
-            font-weight 400
+            font-weight 700
             line-height 24px
             .now
               margin-right 8px
               font-size 14px
-              color rgb(240,20,20)
+              color rgb(240, 20, 20)
             .old
               text-decoration line-through
               font-size 10px
-              color rgb(147,153,159)
+              color rgb(147, 153, 159)
           .cartcontrol-wrapper
             position absolute
             right 0
